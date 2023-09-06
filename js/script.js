57b82eea332301a0e52aa9d77527da126afafe5a
Oscar Gallardo <oscar.gallardo@fceyt.unse.edu.ar>
2023-09-06 00:30:46 -0300

    Se agrego flecha hacia arriba. Se agrego archivo JS para que la flecha me envia hacia la parte superior de la pagina

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// La función scrollToTop() utiliza el método window.scrollTo() para desplazarse hacia la parte superior de la página.
+// La opción top: 0 especifica que queremos desplazarnos hasta la parte superior de la página. 
+// La opción behavior: 'smooth' hace que el desplazamiento sea suave y animado.
+function scrollToTop() {
+
+    window.scrollTo({
+  
+      top: 0,
+  
+      behavior: 'smooth'
+  
+    });
+  
+  }
